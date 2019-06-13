eb26b1c7c38588d8becfcbe6f3ff96e26f7b4de2
ely-uf <elyahove@student.unit.ua>
2019-06-13 22:26:55 +0300

    Add machobj error for invalid iterators.

--- machobj_err.c
+++ machobj_err.c
@@ -1,28 +1,29 @@
 #include "machobj_err.h"
 
 static const char *arr[MO_LAST] = {
-	"All good.",
-	"Failed to open file.",
-	"Failed to map file in memory.",
-	"Failed to allocate memory.",
-	"File seems to be truncated or corrupted.",
-	"File contents are invalid."
+    [MO_OK]             =   "All good.",
+    [MO_OPEN_FAILED]    =   "Failed to open file.",
+    [MO_MMAP_FAILED]    =   "Failed to map file in memory.",
+    [MO_MALLOC_FAILED]  =   "Failed to allocate memory.",
+    [MO_TRUNCATED]      =   "File seems to be truncated or corrupted.",
+    [MO_INVALID]        =   "File contents are invalid.",
+    [MO_ITER_INVALID]   =   "Iterator has not been initialized.",
 };
 
 static t_machobj_err mach_errno;
 const char * mach_err_func;
 long unsigned int mach_err_line;
 
-void	_machobj_set_err(t_machobj_err err, const char *f, long unsigned int l)
+void    _machobj_set_err(t_machobj_err err, const char *f, long unsigned int l)
 {
-	if (err < MO_OK || err >= MO_LAST) 
-		return ;
-	mach_errno = err;
-	mach_err_func = f;
-	mach_err_line = l;
+    if (err < MO_OK || err >= MO_LAST) 
+        return ;
+    mach_errno = err;
+    mach_err_func = f;
+    mach_err_line = l;
 }
 
 const char *machobj_strerr(void)
 {
-	return (arr[mach_errno]);
+    return (arr[mach_errno]);
 }
--- machobj_err.h
+++ machobj_err.h
@@ -1,19 +1,20 @@
 #ifndef __MACHOBJ_ERR_H
 # define __MACHOBJ_ERR_H
 
-typedef enum	e_machobj_err
+typedef enum    e_machobj_err
 {
-	MO_OK,
-	MO_OPEN_FAILED,
-	MO_MMAP_FAILED,
-	MO_MALLOC_FAILED,
-	MO_TRUNCATED,
-	MO_INVALID,
-	MO_LAST
-}				t_machobj_err;
+    MO_OK,
+    MO_OPEN_FAILED,
+    MO_MMAP_FAILED,
+    MO_MALLOC_FAILED,
+    MO_TRUNCATED,
+    MO_INVALID,
+    MO_ITER_INVALID,
+    MO_LAST
+}               t_machobj_err;
 
-void			_machobj_set_err(t_machobj_err e, const char *f, long unsigned int l);
-const char		*machobj_strerr(void);
+void            _machobj_set_err(t_machobj_err e, const char *f, long unsigned int l);
+const char      *machobj_strerr(void);
 
 # define machobj_set_err(_err) _machobj_set_err(_err, __func__, __LINE__)
 
