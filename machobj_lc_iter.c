54152a7d8206c7af0a522cb66611b81c5ee4e0c9
ely-uf <elyahove@student.unit.ua>
2019-06-13 22:31:06 +0300

    Add proper error reporting for lc_iter.

--- machobj_lc_iter.c
+++ machobj_lc_iter.c
@@ -1,29 +1,34 @@
 #include "machobj_lc_iter.h"
+#include "machobj_err.h"
 
-void	machobj_lc_iter_init(lc_iter *iter, void *mem, size_t size)
+void    machobj_lc_iter_init(lc_iter *iter, void *mem, size_t size)
 {
-	*iter = (lc_iter){mem, size, 0};
+    *iter = (lc_iter){mem, size, 0};
 }
 
 struct load_command *machobj_lc_next(lc_iter *iter)
 {
-	struct load_command	*lc;
+    struct load_command *lc;
 
-	if (iter->mem == NULL || iter->offset == iter->size)
-		return NULL;
+    if (iter->mem == NULL || iter->offset == iter->size)
+    {
+        if (iter->mem == NULL)
+            machobj_set_err(MO_ITER_INVALID);
+        return NULL;
+    }
 
-	lc = (struct load_command*)((char*)iter->mem + iter->offset);
-	/*
-	** XXX: Error reporting?
-	*/
-	if (lc->cmdsize > iter->size - iter->offset)
-		return NULL;
+    lc = (struct load_command*)((char*)iter->mem + iter->offset);
+    if (lc->cmdsize > iter->size - iter->offset)
+    {
+        machobj_set_err(MO_TRUNCATED);
+        return NULL;
+    }
 
-	iter->offset += lc->cmdsize;
-	return lc;
+    iter->offset += lc->cmdsize;
+    return lc;
 }
 
-void	machobj_lc_iter_reset(lc_iter *iter)
+void    machobj_lc_iter_reset(lc_iter *iter)
 {
-	iter->offset = 0;
+    iter->offset = 0;
 }
--- machobj_lc_iter.h
+++ machobj_lc_iter.h
@@ -6,15 +6,15 @@
 
 # define LC_ITER_INITIALIZER ((lc_iter) { NULL, 0, 0 })
 
-typedef struct	s_lc_iter
+typedef struct  s_lc_iter
 {
-	void	*mem;
-	size_t	size;
-	size_t	offset;
-}				lc_iter;
+    void    *mem;
+    size_t  size;
+    size_t  offset;
+}               lc_iter;
 
-void				machobj_lc_iter_init(lc_iter *iter, void *mem, size_t size);
+void                machobj_lc_iter_init(lc_iter *iter, void *mem, size_t size);
 struct load_command *machobj_lc_next(lc_iter *iter);
-void				machobj_lc_iter_reset(lc_iter *iter);
+void                machobj_lc_iter_reset(lc_iter *iter);
 
 #endif
