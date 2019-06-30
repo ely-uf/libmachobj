37334ffe087f31c9ef5b2a909d2448634b597730
ely-uf <elyahove@student.unit.ua>
2019-06-30 15:45:37 +0300

    Harden the work with strtable. Add safer API.

--- machobj_symtab_strtable.c
+++ machobj_symtab_strtable.c
@@ -1,6 +1,8 @@
 #include "machobj_symtab_strtable.h"
 #include "machobj_utils.h"
 #include "machobj_err.h"
+#include <stdlib.h>
+#include <string.h>
 
 const char  *machobj_symtab_strtable_get(t_machobj *obj,
                                          struct symtab_command *sc,
@@ -10,9 +12,9 @@ const char  *machobj_symtab_strtable_get(t_machobj *obj,
     uint32_t    stroff = MOBJ_GET32(obj, sc->stroff);
     uint32_t    strsize = MOBJ_GET32(obj, sc->strsize);
 
-    if (lookup_idx == 0)
+    if (lookup_idx <= 0)
         return "";
-    if (stroff + strsize > obj->size)
+    if (stroff + strsize > obj->size || stroff + lookup_idx > obj->size)
     {
         machobj_set_err(MO_TRUNCATED);
         return NULL;
@@ -21,4 +23,67 @@ const char  *machobj_symtab_strtable_get(t_machobj *obj,
     strtable = (char*)obj->data + stroff;
 
     return &strtable[lookup_idx];
+}
+
+size_t          machobj_symtab_strtable_strlen(t_machobj *obj,
+                                               struct symtab_command *sc,
+                                               long lookup_idx)
+{
+    const char      *strtable;
+    const uint32_t  stroff = MOBJ_GET32(obj, sc->stroff);
+    const uint32_t  strsize = MOBJ_GET32(obj, sc->strsize);
+    size_t          i;
+
+    if (lookup_idx <= 0)
+        return 0;
+    if (stroff + strsize > obj->size || stroff + lookup_idx > obj->size)
+        return 0;
+
+    strtable = (char*)obj->data + stroff;
+    i = lookup_idx;
+    while ((strtable - (char*)obj->data + i) < obj->size && strtable[i])
+        i++;
+
+    return i - lookup_idx;
+}
+
+int             machobj_symtab_strtable_get_safe(t_machobj *obj,
+                                                 struct symtab_command *sc,
+                                                 long lookup_idx,
+                                                 char *buf,
+                                                 size_t buflen)
+{
+    const char      *entry = machobj_symtab_strtable_get(obj, sc, lookup_idx);
+    const size_t    len = machobj_symtab_strtable_strlen(obj, sc, lookup_idx);
+    const size_t    size = len < buflen ? len : buflen ;
+
+    if (!entry || !buf || !buflen)
+        return 1;
+
+    strncpy(buf, entry, size);
+    buf[size] = 0;
+
+    return 0;
+}
+
+char            *machobj_symtab_strtable_get_safe_m(t_machobj *obj,
+                                                    struct symtab_command *sc,
+                                                    long lookup_idx)
+{
+    const uint32_t  size = machobj_symtab_strtable_strlen(obj, sc, lookup_idx);
+    char            *buf = (char*)malloc(size + 1);
+
+    if (!buf)
+    {
+        machobj_set_err(MO_MALLOC_FAILED);
+        return NULL;
+    }
+
+    if (machobj_symtab_strtable_get_safe(obj, sc, lookup_idx, buf, size))
+    {
+        free(buf);
+        return NULL;
+    }
+
+    return buf;
 }
--- machobj_symtab_strtable.h
+++ machobj_symtab_strtable.h
@@ -7,4 +7,14 @@ const char  *machobj_symtab_strtable_get(t_machobj *obj,
                                          struct symtab_command *sc,
                                          long idx);
 
+int         machobj_symtab_strtable_get_safe(t_machobj *obj,
+                                             struct symtab_command *sc,
+                                             long idx,
+                                             char *buf,
+                                             size_t bufsize);
+
+char        *machobj_symtab_strtable_get_safe_m(t_machobj *obj,
+                                                struct symtab_command *sc,
+                                                long idx);
+
 #endif
